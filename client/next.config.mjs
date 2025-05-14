9f0e04f7a88468902d794ea317a4a633717a5652
Tanish Bhole <tanishbhole@gmail.com>
2025-05-15 04:00:05 +0530

    push

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,11 +1,4 @@
-Lack of real-time updates in stock levels
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-Manual data entry, leading to human errors
-
-Disjointed systems for sales, purchases, and expenses
-
-Poor reporting tools and limited forecasting features
-
-Inadequate scalability and integration with modern cloud infrastructure
-
-The absence of a unified, real-time, and scalable system limits business growth and reduces operational efficiency. Munshi is designed to solve these issues by offering a comprehensive platform built with modern technologies.
+export default nextConfig;
